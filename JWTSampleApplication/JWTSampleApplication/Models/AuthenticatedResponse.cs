9bed1de7956aba9fe9d005f07ce873b21edd615a
Nagaraj Natarajan <nagaraj.natarajan@boeing.com>
2022-05-07 13:17:04 +0530

    JWT - Sample Application

---
+++ JWTSampleApplication/JWTSampleApplication/Models/AuthenticatedResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTSampleApplication.Models
+{
+    public class AuthenticatedResponse
+    {
+        public string? Token { get; set; }
+    }
+}
